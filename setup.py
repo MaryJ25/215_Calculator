aa7d29741d13aaef2022d9c1220718fa77fec69c
Mary Czapkowska <43907321+MaryJ25@users.noreply.github.com>
2021-03-15 17:10:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="calculator",
     version="0.0.1",
     author="Maria Capkovska",
     author_email="maria.hej253@gmail.com",
     description="Calculator package made as an exercise in a Data Science course",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/MaryJ25/215_Calculator/",
     classifiers=[
         "Programming Language :: Python :: 3",
