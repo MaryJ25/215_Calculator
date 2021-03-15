415577347704d8f66c04782c9fd0a6772c69309c
Mary Czapkowska <43907321+MaryJ25@users.noreply.github.com>
2021-03-15 17:02:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Calculator",
+    name="calculator",
     version="0.0.1",
     author="Maria Capkovska",
     author_email="maria.hej253@gmail.com",
